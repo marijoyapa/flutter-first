9c844f1d3dfde7a483e1f27b16fa6dc7ab6d1691
marijoyapa <137674934+marijoyapa@users.noreply.github.com>
2023-10-03 02:49:44 +0800

    roll dice

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/text.dart';
+import 'package:first_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.color});
+
+  final List<Color> color;
+
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: color,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
